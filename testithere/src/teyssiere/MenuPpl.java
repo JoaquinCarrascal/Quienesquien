d6a710b873992f8c4df07f40e7262ab9ac5d8fbe
Pablo Teyssiere <teyssiere.lupab24@triana.salesianos.edu>
2023-12-11 12:47:51 +0100

    Teyssiere

---
+++ testithere/src/teyssiere/MenuPpl.java
@@ -0,0 +1,29 @@
+package teyssiere;
+
+public class MenuPpl {
+
+	public static void main(String[] args) {
+		
+		//Menu para elegir el tablero (3x3, 4x4, 5x5)
+		//Menu de juego con: leyenda (explicacion)
+		//Segun el juego va a tener unas preguntas específicas, por ejemplo
+		/*
+		 * Leyenda:
+		 * En el panel de personas que se mostrará a continuación saldrán abreviaciones
+		 * según las características de la persona.
+		 * Estas serán las abreviaciones que se mostraran:
+		 * Sexo:Hombre(M), Mujer(F).
+		 * Continente:Europa (EU),América(AM),Asia(AS),África(AF),Oceanía(OC).
+		 * Pelo:Moreno (Mor),Rubio(Rub),Castaño(Cas),Pelirojo(Roj),Calvo(Cal).
+		 * Ojos: Marrones(Mar),Azules(Azu),Verdes(Ver).
+		 * 
+		 * Para formular las preguntas se harán de esta forma:
+		 * ¿Es (hombre/mujer o oceanico/europeo/etc )?
+		 * ¿Tiene (el pelo/los ojos) (moreno.../azules...)
+		 * ¿Mide (mas/menos) "altura"?*/
+		
+
+	
+	}
+
+}
